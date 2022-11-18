07916515c05dfbe6c642b076bda2daa506feb48c
Krishna Vadavi <krishnavadavi@krishnas-mini.lan>
2022-11-18 01:52:08 -0600

    final updates

---
+++ README.md
@@ -0,0 +1,34 @@
+# Technical Quiz
+
+This repo hosts quiz app which is based on JavaScript. In order to run the applicaiton. Clone the repo to your local machine and open `index.html` in your browser.
+
+## How to play Quiz and its rules.
+
+- Click on the `Start Quiz ` Button.
+- Timer starts for 60 seconds. Try to finish the quiz within given timeframe.
+- Select the Radio button for the one user feels its correct.
+- Once selected, click on `Next Question` Button for next question and `result` of current question.
+- For every incorrect answer, 10 seconds will be deducted from the timer. 
+- Once timer is over, Game is over and user can check the stats from `View HighScore` 
+
+## Screenshots of application.
+
+- Start the Quiz
+
+![start](./etc/images/start.png)
+
+- Correct Answer View
+
+![correct](./etc/images/correct.png)
+
+- Wrong Answer View with time deduction.
+
+![wrong](./etc/images/wrong.png)
+
+- Game over.
+
+![gameover](./etc/images/gameover.png)
+
+- Highscore/Stats.
+
+![highscore](./etc/images/highscore.png)
--- src/main.js
+++ src/main.js
@@ -5,23 +5,29 @@ var options = document.querySelector("#options");
 var aOption = document.getElementById('a');
 var bOption = document.getElementById('b');
 var cOption = document.getElementById('c');
+var aaOption = document.getElementById('aa');
+var bbOption = document.getElementById('bb');
+var ccOption = document.getElementById('cc');
 var mainEl = document.querySelector("#main");
 var start = document.getElementById("start");
 var count
-// var correctAnswer = 0
-var wrongAnswer = 0
+var correctAnswerCount = 0
+var wrongAnswerCount = 0
 var message = "Time Up! Game Over."
 var words = message
 var timeLeft = 0;
 var timeInterval = 0;
+var ans = "";
+var game = 1;
 
 // Set button value to Start Quiz
 start.textContent = "Start Quiz"
 document.getElementById('button').style.visibility='hidden';
+document.getElementById('aa').style.visibility='hidden';
+document.getElementById('bb').style.visibility='hidden';
+document.getElementById('cc').style.visibility='hidden';
+
 // Add list of questions in a variable.
-
-
-
 const myQuestions = [
   {
     question: "Who invented JavaScript?",
@@ -46,20 +52,47 @@ const myQuestions = [
     answers: {
       a: "Angular",
       b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint"
+      c: "ESLint"
     },
-    correctAnswer: "d"
-  }
+    correctAnswer: "c"
+  },
+  {
+    question: "What does HTML stand for?",
+    answers: {
+      a : "HyperText Markup Language",
+      b : "HyperLinks and Text Markup Language",
+      c : "Home Tool Markup Language"
+    },
+    correctAnswer: "a"
+  },
+    {
+      question: "Who is making the Web standards?",
+      answers: {
+        a : "Google",
+        b : "Microsoft",
+        c : "The World Wide Web Consortium"
+      },
+      correctAnswer: "c"
+    },
+     {
+        question: "Which character is used to indicate an end tag?",
+        answers: {
+          a : "<",
+          b : "/",
+          c : "*"
+        },
+        correctAnswer: "b"
+      }
 ];
 
 
 
-
+// Function to wait for next button click and question.
 let waitForPressResolve;
 
 function waitForPress() {
     return new Promise(resolve => waitForPressResolve = resolve);
+    
 }
 
 const btn = document.querySelector("#button");
@@ -68,33 +101,114 @@ function btnResolver() {
   if (waitForPressResolve) waitForPressResolve();
 }
 
+// function to place next question and break if time left is less than or equal to 0
 async function doIt() {
   btn.addEventListener('click', btnResolver);
   document.getElementById('button').style.visibility='visible';
+  document.getElementById('aa').style.visibility='visible';
+  document.getElementById('bb').style.visibility='visible';
+  document.getElementById('cc').style.visibility='visible';
 
-  for (var i=0; i < myQuestions.length; i++ ) {    
+  for (var i=0; i < myQuestions.length; i++ ) {
+    if (timeLeft <= 0) {
+      break
+    }    
       question.innerHTML = myQuestions[i].question
       aOption.innerHTML = myQuestions[i].answers["a"]
       bOption.innerHTML = myQuestions[i].answers["b"]
       cOption.innerHTML = myQuestions[i].answers["c"]
+      ans = myQuestions[i].correctAnswer
+
       await waitForPress();
+      if (timeLeft <= 0) {
+        break
+      }  
+      if (ans === document.querySelector('input[name="radioButton"]:checked').value){
+        console.log("Correct")
+        correctAnswerCount ++
+        mainEl.textContent = "Correct Answer!"
+      } else {
+        wrongAnswerCount ++
+        timerClock()
+        mainEl.textContent = "Wrong Answer!"
+
+      }
+      aaOption.checked = false
+      bbOption.checked = false
+      ccOption.checked = false
 
   }
   btn.removeEventListener('click', btnResolver);
   console.log('Finished');
+  timerEl.textContent = ""
+  question.textContent = "Game Over!"
+  aOption.textContent = ""
+  bOption.textContent = ""
+  cOption.textContent = ""
+  mainEl.textContent = ""
 
+  document.getElementById('start').style.visibility='visible';
+  document.getElementById('highscore').style.visibility='visible';
+  start.textContent = "Start Quiz"
+  document.getElementById('button').style.visibility='hidden';
+  document.getElementById('aa').style.visibility='hidden';
+  document.getElementById('bb').style.visibility='hidden';
+  document.getElementById('cc').style.visibility='hidden';
+  clearInterval(timeInterval)
+
+  let jsonOutput = {
+    "Correct" : correctAnswerCount,
+    "Wrong"   : wrongAnswerCount,
+    "Game"    : game++
+  }
+  localStorage.setItem('Score', JSON.stringify(jsonOutput));
 }
 
 
+// Timer function
+function timerClock() {
+
+  if (timeLeft < 0) {
+    clearInterval(timeInterval)
+    final()
+  }
+  timeLeft = timeLeft - 10
+  console.log(timeLeft)
+}
+
+// Final call function to end quiz
+function final(){
+  myQuestions.length = 0
+  timerEl.textContent = ""
+  question.textContent = "Game Over!"
+  aOption.innerHTML = ""
+  bOption.innerHTML = ""
+  cOption.innerHTML = ""
+  mainEl.innerHTML = ""
+  document.getElementById('start').style.visibility='visible';
+  document.getElementById('highscore').style.visibility='visible';
+  start.textContent = "Start Quiz"
+  document.getElementById('button').style.visibility='hidden';
+  document.getElementById('aa').style.visibility='hidden';
+  document.getElementById('bb').style.visibility='hidden';
+  document.getElementById('cc').style.visibility='hidden';
+  clearInterval(timeInterval)
+}
+
+//Alert for high score
+function highScore() {
+  let highScores = localStorage.getItem('Score')
+  alert(highScores)
+  }
 
 
+  highscore.addEventListener("click", function (event) {
+    event.preventDefault();
+    highScore()
+});
 
 
-
-
-
-
-
+// count down function
 function countdown() {
   document.getElementById('start').style.visibility='hidden';
   document.getElementById('highscore').style.visibility='hidden';
@@ -115,41 +229,7 @@ function countdown() {
   }, 1000);
 }
 
-
-
-
-
-// myQuestions.forEach(i => {
-//   var button = document.createElement("button");
-//   button.innerText = i;
-  
-//   button.addEventListener("click", function() {
-//     console.log(i.question)
-//   })
-//   aOption.appendChild(button);
-// } )
-
-
-function questionslist(){
-  for (var i=0; i < myQuestions.length; i++ ) {
-    // console.log(myQuestions[i].question)
-    // console.log(myQuestions[i].correctAnswer)
-    // console.log(myQuestions[i].answers)
-    try {
-      question.innerHTML = myQuestions[i].question
-      aOption.innerHTML = myQuestions[i].answers["a"]
-      bOption.innerHTML = myQuestions[i].answers["b"]
-      cOption.innerHTML = myQuestions[i].answers["c"]
-    } catch(e) {
-      console.log("ERROR")
-  }
-  }
-}
-
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
-
+// Start button or quiz
 start.addEventListener("click", function() {
     count ++
     countdown();
